337ad260166864e67c1a333d0a847bb83e5645e9
Sagitarian812 <62075331+Sagitarian812@users.noreply.github.com>
2020-11-21 18:28:53 -0500

    Initial commit

---
+++ Assets/Robot Kyle/Assets_ARFoundation1/Scripts/SliderControl.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SliderControl : MonoBehaviour
+{
+    public Slider scaleSlide;
+    public Slider rotation;
+    public GameObject Kyle;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rotation.maxValue = 360;
+        rotation.minValue = 0;
+        scaleSlide.minValue = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Kyle.transform.rotation = Quaternion.Euler(0, rotation.value, 0);
+        Kyle.transform.localScale = new Vector3(scaleSlide.value, scaleSlide.value, scaleSlide.value);
+
+    }
+}
